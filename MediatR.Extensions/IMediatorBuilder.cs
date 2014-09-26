94d11cb81579345c8cee68068a52a2fdc4e10d66
Brendan McMahon <bernos@gmail.com>
2014-09-26 12:25:16 +1000

    Added interface and abstract impelmentation of mediator builder

---
+++ MediatR.Extensions/IMediatorBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+
+namespace MediatR.Extensions
+{
+    public interface IMediatorBuilder
+    {
+        IMediatorBuilder WithRequestDecorator(string name, Type decoratorType);
+        IMediatorBuilder WithRequestHandlerAssemblies(params Assembly[] assemblies);
+        IMediator Build();
+    }
+}
---
+++ MediatR.Extensions/MediatorBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace MediatR.Extensions
+{
+    public abstract class MediatorBuilder : IMediatorBuilder
+    {
+        private bool _isBuilt;
+
+        public IMediatorBuilder WithRequestDecorator(string name, Type decoratorType)
+        {
+            if (_isBuilt)
+            {
+                throw new Exception("Cannot call AddRequestDecorator after Build() has been called");
+            }
+
+            if (decoratorType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IAsyncRequestHandler<,>)))
+            {
+                RegisterAsyncRequestDecorator(name, decoratorType);
+            }
+            else if (
+                decoratorType.GetInterfaces()
+                    .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)))
+            {
+                RegisterRequestDecorator(name, decoratorType);
+            }
+            else
+            {
+                throw new ArgumentException("Decorator type must implement IRequestHandler<TRequest,TResponse> or IAsyncRequestHandler<TRequest, TResponse>", "decoratorType");
+            }
+
+            return this;
+        }
+
+        public IMediatorBuilder WithRequestHandlerAssemblies(params Assembly[] assemblies)
+        {
+            if (_isBuilt)
+            {
+                throw new Exception("Cannot call AddRequestDecorator after Build() has been called");
+            }
+
+            foreach (var assembly in assemblies)
+            {
+                RegisterRequestHandlersFromAssembly(assembly);
+                RegisterAsyncRequestHandlersFromAssembly(assembly);
+            }
+
+            return this;
+        }
+
+        public IMediator Build()
+        {
+            if (_isBuilt)
+            {
+                throw new Exception("Build() can only be called once");
+            }
+
+            var mediator = BuildMediator();
+
+            _isBuilt = true;
+
+            return mediator;
+        }
+
+        protected abstract void RegisterRequestDecorator(string name, Type decoratorType);
+        protected abstract void RegisterAsyncRequestDecorator(string name, Type decoratorType);
+        protected abstract void RegisterRequestHandlersFromAssembly(Assembly assembly);
+        protected abstract void RegisterAsyncRequestHandlersFromAssembly(Assembly assembly);
+        protected abstract IMediator BuildMediator();
+    }
+}

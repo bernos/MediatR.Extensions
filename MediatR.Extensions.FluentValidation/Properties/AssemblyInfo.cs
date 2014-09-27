1842dfb141c71aed77e6c015f61b2474eb0caa5f
Brendan McMahon <bernos@gmail.com>
2014-09-27 12:44:30 +1000

    Added fluent validation project

--- MediatR.Extensions.FluentValidation/Properties/AssemblyInfo.cs
+++ MediatR.Extensions.FluentValidation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediatR.Extensions.Autofac")]
-[assembly: AssemblyDescription("Autofac implementation of IMediatorBuilder")]
+[assembly: AssemblyTitle("MediatR.Extensions.FluentValidation")]
+[assembly: AssemblyDescription("Mediator request decorator to make integrating FluentValidation with MediatR simple")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Brendan McMahon")]
-[assembly: AssemblyProduct("MediatR.Extensions.Autofac")]
+[assembly: AssemblyProduct("MediatR.Extensions.FluentValidation")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4b906fd4-eefa-480d-a01e-4af18bb2f8e6")]
+[assembly: Guid("7e3d92b2-c973-44c8-a5d1-23376fd0d664")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.2")]
-[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
---
+++ NuGetPack.bat
@@ -0,0 +1,3 @@
+call nuget pack -sym .\MediatR.Extensions\MediatR.Extensions.csproj -Prop Configuration=Release
+call nuget pack -sym .\MediatR.Extensions.Autofac\MediatR.Extensions.Autofac.csproj -Prop Configuration=Release
+call nuget pack -sym .\MediatR.Extensions.FluentValidation\MediatR.Extensions.FluentValidation.csproj -Prop Configuration=Release

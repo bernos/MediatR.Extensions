6be88dc5ee801d7800c8e54ec2495b6e245ea1ff
Brendan McMahon <bernos@gmail.com>
2014-10-19 20:46:29 +1100

    Fixed bug with 'like' pattern

--- Set-PackageVersion.ps1
+++ Set-PackageVersion.ps1
@@ -7,7 +7,7 @@ Get-ChildItem -Recurse -Force | Where-Object { $_.Name -eq "AssemblyInfo.cs" } |
     } | Set-Content $_.FullName -Encoding UTF8
 }
 
-Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like ".nuspec" } | ForEach-Object {
+Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like "*.nuspec" } | ForEach-Object {
 	(Get-Content $_.FullName) | ForEach-Object {
 		($_ -replace '<dependency id="MediatR.Extensions" version=".*"/>', ('<dependency id="MediatR.Extensions" version="' + $v + '"/>'))
 	} | Set-Content $_.FullName -Encoding UTF8

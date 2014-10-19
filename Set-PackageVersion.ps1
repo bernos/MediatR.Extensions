3781c836b2cb1ee93450ecadf89e8b766c8edc40
Brendan McMahon <bernos@gmail.com>
2014-10-19 20:32:07 +1100

    Added nuget helper scripts

---
+++ Set-PackageVersion.ps1
@@ -0,0 +1,8 @@
+﻿
+$v = $args[0]
+
+Get-ChildItem -Recurse -Force | Where-Object { $_.Name -eq "AssemblyInfo.cs" } | ForEach-Object {
+    (Get-Content $_.FullName) | ForEach-Object {
+        ($_ -replace 'AssemblyVersion\(.*\)', ('AssemblyVersion("' + $v + '")')) -replace 'AssemblyFileVersion\(.*\)', ('AssemblyFileVersion("' + $v + '")')
+    } | Set-Content $_.FullName -Encoding UTF8
+}

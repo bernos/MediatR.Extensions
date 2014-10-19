7af3d9d1b687860704c20bebe339444c077a9c08
Brendan McMahon <bernos@gmail.com>
2014-10-19 20:44:13 +1100

    Set-PackageVersion will now update dependency versions

--- Set-PackageVersion.ps1
+++ Set-PackageVersion.ps1
@@ -6,3 +6,9 @@ Get-ChildItem -Recurse -Force | Where-Object { $_.Name -eq "AssemblyInfo.cs" } |
         ($_ -replace 'AssemblyVersion\(.*\)', ('AssemblyVersion("' + $v + '")')) -replace 'AssemblyFileVersion\(.*\)', ('AssemblyFileVersion("' + $v + '")')
     } | Set-Content $_.FullName -Encoding UTF8
 }
+
+Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like ".nuspec" } | ForEach-Object {
+	(Get-Content $_.FullName) | ForEach-Object {
+		($_ -replace '<dependency id="MediatR.Extensions" version=".*"/>', ('<dependency id="MediatR.Extensions" version="' + $v + '"/>'))
+	} | Set-Content $_.FullName -Encoding UTF8
+}

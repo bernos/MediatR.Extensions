3bf011068e6be6948d74e0ac5ff740a81dd79fcf
Brendan McMahon <bernos@gmail.com>
2014-10-19 20:23:46 +1100

    Added extension methods to IMediatorBuilder to make make registration of Log4Net and FluentValidation request handlers straightforward

---
+++ MediatR.Extensions.FluentValidation/MediatorBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace MediatR.Extensions.FluentValidation
+{
+    public static class MediatorBuilderExtensions
+    {
+        public static void UseFluentValidation(this IMediatorBuilder builder)
+        {
+            builder.WithRequestDecorator("FluentValidation", typeof (ValidationRequestHandler<,>));
+            builder.WithRequestDecorator("FluentValidationAsync", typeof (AsyncValidationRequestHandler<,>));
+        }
+    }
+}
---
+++ MediatR.Extensions.log4net/MediatorBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace MediatR.Extensions.log4net
+{
+    public static class MediatorBuilderExtensions
+    {
+        public static void UseLog4Net(this IMediatorBuilder builder)
+        {
+            builder.WithRequestDecorator("Log4Net", typeof (LoggingRequestHandler<,>));
+            builder.WithRequestDecorator("Log4NetAsync", typeof (AsyncLoggingRequestHandler<,>));
+        }
+    }
+}
